fda909904d4a72b796f26fb913d2c1caf2529ddd
PriyankaDaptare <71229459+PriyankaDaptare@users.noreply.github.com>
2020-09-15 11:10:16 +0530

    Update contact-details.component.ts

--- src/app/contacts/contact-details/contact-details.component.ts
+++ src/app/contacts/contact-details/contact-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-details',
@@ -8,9 +12,9 @@ import { Component, OnInit } from '@angular/core';
 export class ContactDetailsComponent implements OnInit {
   contactDetails = [
     { name: 'Raghav Guarv', number: 7857497588 , jobTitle: 'Developer'},
-    { name: 'Raman Sharma', number: 8857584895 , jobTitle: 'Developer'},
-    { name: 'Pooja Shaha', number: 90876789578 , jobTitle: 'Developer'},
-    { name: 'Priyanka Dange', number: 876903836 , jobTitle: 'Developer'},
+    { name: 'Raman Sharma', number: 8857584895 , jobTitle: 'Consulatant'},
+    { name: 'Pooja Shaha', number: 90876789578 , jobTitle: 'HR'},
+    { name: 'Priyanka Dange', number: 876903836 , jobTitle: 'Node Developer'},
     { name: 'Jay Bohra', number: 9364665748 , jobTitle: 'Manager'},
   ];
   contactNameDetails = [
@@ -22,21 +26,30 @@ export class ContactDetailsComponent implements OnInit {
   contacNumbertDetails = [
     '7857497588', '8857584895', '90876789578', '876903836', '9364665748'
    ];
-   allContactDetails: any;
-  constructor() { }
+  allContactDetails: any;
+  isLoading: boolean = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.allContactDetails = this.contactDetails;
   }
 
+  /* Search by Name or job title */
   applyFilter(filter: String) {
+    this.isLoading = true;
     this.contactDetails  = this.allContactDetails.filter(item => {
-          if (item.name.toString().toLowerCase().indexOf(filter.toLowerCase()) !== -1) {
+          if (item.name.toString().toLowerCase().indexOf(filter.toLowerCase()) !== -1 ||
+          item.jobTitle.toString().toLowerCase().indexOf(filter.toLowerCase()) !== -1) {
+            this.isLoading = false;
             return true;
           }
             return false;
     }
     );
-}
+ }
 
+  onBackClick(){
+  this.router.navigate(['/account-details']);
+ }
 }
